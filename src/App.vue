7ff4e8f20148411789bb25eff0f678e3dd3bea62
Sjoerd van der Meer <sjoerd.meer@outlook.com>
2017-04-25 15:53:15 +0200

    Add scss

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,15 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './scss/variables';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $primary-color;
   margin-top: 60px;
 }
 </style>
